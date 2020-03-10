67386cf8049e8ececd4f10fde4bbbc0057aa253d
StancuAndrei <57097937+StancuAndrei@users.noreply.github.com>
2020-03-10 12:11:22 +0200

    Updates

---
+++ precache-manifest.8392119dc2f9028b25898949b66f4410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7000435c579b97bc02be57081d88bd",
+    "url": "/Sorting-Project/index.html"
+  },
+  {
+    "revision": "75bce223594ace5dde7f",
+    "url": "/Sorting-Project/static/css/main.4570c5cc.chunk.css"
+  },
+  {
+    "revision": "b8f5701530ea8400af25",
+    "url": "/Sorting-Project/static/js/2.f82c95bb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Sorting-Project/static/js/2.f82c95bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bce223594ace5dde7f",
+    "url": "/Sorting-Project/static/js/main.e451cb83.chunk.js"
+  },
+  {
+    "revision": "4af7102c305812fb99ad",
+    "url": "/Sorting-Project/static/js/runtime-main.135ed337.js"
+  }
+]);
